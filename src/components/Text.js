b79bcbec31c8340b3f052ecd43b41636f6750e14
Brendon Smith <brendonsmith@seacloud9.org>
2021-07-04 12:30:58 -0700

    fixing racing condition

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,19 +1,26 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
 import { useThree } from 'react-three-fiber'
 import { a } from 'react-spring/three'
-//import './Text.css'
-const font = require("../assets/fonts/Road_Rage.ttf")
-const neonFont = new FontFace('Road Rage', `url(${font})`);
-
-neonFont.load().then(function(loadedFont) {
-  document.fonts.add(loadedFont)
-}).catch(function(error) {
-  console.log('Failed to load font: ' + error)
-})
+const font = require('../assets/fonts/Road_Rage.ttf')
+const roadRage = new FontFace('RoadRage', `url(${font})`)
 
 /** This renders text via canvas and projects it as a sprite */
 
 function Text({ children, position, opacity, color = 'white', fontSize = 300 }) {
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => {
+    if (!loaded) {
+      roadRage
+        .load()
+        .then((loadedFont) => {
+          document.fonts.add(loadedFont)
+          setLoaded(true)
+        })
+        .catch(function (error) {
+          console.log('Failed to load font: ' + error)
+        })
+    }
+  }, [loaded])
   const {
     size: { width, height },
     viewport: { width: viewportWidth, height: viewportHeight }
@@ -23,27 +30,27 @@ function Text({ children, position, opacity, color = 'white', fontSize = 300 }) 
     const canvas = document.createElement('canvas')
     canvas.width = canvas.height = 2048
     const context = canvas.getContext('2d')
-    
-    context.font = `${fontSize}px 'Road Rage'`
+    context.font = `${fontSize}px 'RoadRage'`
     context.textAlign = 'center'
     context.textBaseline = 'middle'
-    var grd = context.createLinearGradient(0,0,0,canvas.width);
-    grd.addColorStop(0,"#FF00BF");
-    grd.addColorStop(0.3,"#DFFF00");
-    grd.addColorStop(0.6,"#470E6B");
-    grd.addColorStop(0.1,"#FF00BF");
-    
-    context.fillStyle = grd;
+    var grd = context.createLinearGradient(0, 0, 0, canvas.width)
+    grd.addColorStop(0, '#FF00BF')
+    grd.addColorStop(0.3, '#DFFF00')
+    grd.addColorStop(0.6, '#470E6B')
+    grd.addColorStop(0.1, '#FF00BF')
+    context.fillStyle = grd
     context.fillText(children, 1024, 1024 - 410 / 2)
     return canvas
-  }, [children, width, height, color, fontSize])
+  }, [children, width, height, color, fontSize, loaded])
 
   return (
-    <a.sprite scale={[scale / 2, scale / 2, 1]} position={position}>
-      <a.spriteMaterial attach="material" transparent opacity={opacity}>
-        <canvasTexture attach="map" image={canvas} premultiplyAlpha onUpdate={s => (s.needsUpdate = true)} />
-      </a.spriteMaterial>
-    </a.sprite>
+    loaded && (
+      <a.sprite scale={[scale / 2, scale / 2, 1]} position={position}>
+        <a.spriteMaterial attach="material" transparent opacity={opacity}>
+          <canvasTexture attach="map" image={canvas} premultiplyAlpha onUpdate={(s) => (s.needsUpdate = true)} />
+        </a.spriteMaterial>
+      </a.sprite>
+    )
   )
 }
 
